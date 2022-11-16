2823578686c123b4ddd840930128cac0557f5c2a
Evelyn Valles <106193803+evelynvalles@users.noreply.github.com>
2022-11-15 18:13:53 -0600

    Time: 0 ms (100.00%), Space: 42.5 MB (51.68%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+     
+        int next = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[next] = nums[i];
+                next++;
+            }
+        }
+        return next;
+    }
+}
